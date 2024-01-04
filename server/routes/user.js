0621a7a1eaa399f888b1b2c238f7e05de4046408
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2024-01-04 23:02:55 +0100

    Sign up WIP

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
+// controller functions
+const { loginUser, signupUser } = require("../controllers/userController");
+
 const router = express.Router();
 
 // login route
-router.post("/login", (req, res) => {});
-
+router.post("/login", loginUser);
 // signup route
-router.post("/signup", (req, res) => {});
+router.post("/signup", signupUser);
 
 module.exports = router;
