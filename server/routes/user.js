d17bafc07541df3c9004b03de4a5d374f357b28c
Mikołaj Sławnikowski <mikolaj.slawnikowski@gmail.com>
2024-01-04 22:36:09 +0100

    Initial commit

---
+++ server/routes/user.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+// login route
+router.post("/login", (req, res) => {});
+
+// signup route
+router.post("/signup", (req, res) => {});
+
+module.exports = router;
